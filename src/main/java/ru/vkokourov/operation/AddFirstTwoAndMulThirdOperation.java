b62f44e3754bc86fdc5c29ff56ea254b21c3254e
vkoko <@vkokourov>
2023-10-20 17:09:59 +0700

    Add validate number of args

--- src/main/java/ru/vkokourov/operation/AddFirstTwoAndMulThirdOperation.java
+++ src/main/java/ru/vkokourov/operation/AddFirstTwoAndMulThirdOperation.java
@@ -1,6 +1,16 @@
 package ru.vkokourov.operation;
 
+import java.util.List;
+
 public class AddFirstTwoAndMulThirdOperation extends Operation {
+
+    @Override
+    public void setArgs(List<Long> args) {
+        super.setArgs(args);
+        if (args.size() != 3) {
+            throw new RuntimeException("Invalid number of arguments: " + args.size());
+        }
+    }
 
     @Override
     public Long calcResult() {
