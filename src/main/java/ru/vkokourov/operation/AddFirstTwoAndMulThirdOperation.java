101ad6caf26bbe9e20bbae1f390d999ca9a511ce
vkoko <@vkokourov>
2023-10-20 10:33:58 +0700

    Implement add first two and mul third operation

--- src/main/java/ru/vkokourov/operation/AddFirstTwoAndMulThirdOperation.java
+++ src/main/java/ru/vkokourov/operation/AddFirstTwoAndMulThirdOperation.java
@@ -6,10 +6,13 @@ public class AddFirstTwoAndMulThirdOperation extends ArithmeticOperation {
 
     public AddFirstTwoAndMulThirdOperation(List<Long> args) {
         super(args);
+        if (args.size() != 3) {
+            throw new RuntimeException("Invalid number of arguments");
+        }
     }
 
     @Override
     public Long calcResult() {
-        return null;
+        return (args.get(0) + args.get(1)) * args.get(2);
     }
 }
